dd82f6b0e6054f3753aee4ac638f9c20b0a835f5
domthomas-dev <domthomas-dev@users.noreply.github.com>
2024-08-02 13:04:21 +0000

    Fix styling

--- src/Listeners/HormLogResponse.php
+++ src/Listeners/HormLogResponse.php
@@ -11,11 +11,11 @@ class HormLogResponse
 
     public function handle(ResponseReceived $response)
     {
-//        Log::info('Response received', [
-//            'url' => $response->request->url(),
-//            'status' => $response->response->status(),
-//            'body' => $response->response->body(),
-//        ]);
-        Log::info('serializé : ' . base64_encode(serialize($response->response)));
+        //        Log::info('Response received', [
+        //            'url' => $response->request->url(),
+        //            'status' => $response->response->status(),
+        //            'body' => $response->response->body(),
+        //        ]);
+        Log::info('serializé : '.base64_encode(serialize($response->response)));
     }
 }
