cc637082f9a420d57054e69fbf1ea096646bc9e9
Dominique Thomas <dthomas@codenco.fr>
2024-08-02 15:03:51 +0200

    wip save entries

--- src/Listeners/HormLogResponse.php
+++ src/Listeners/HormLogResponse.php
@@ -3,10 +3,19 @@
 namespace NcooDev\HormLogger\Listeners;
 
 use Illuminate\Http\Client\Events\ResponseReceived;
+use Illuminate\Support\Facades\Log;
 
 class HormLogResponse
 {
     public function __construct() {}
 
-    public function handle(ResponseReceived $response) {}
+    public function handle(ResponseReceived $response)
+    {
+//        Log::info('Response received', [
+//            'url' => $response->request->url(),
+//            'status' => $response->response->status(),
+//            'body' => $response->response->body(),
+//        ]);
+        Log::info('serializé : ' . base64_encode(serialize($response->response)));
+    }
 }
