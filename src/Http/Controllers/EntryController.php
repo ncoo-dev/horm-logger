802df71575285659293497245a1ce7a164981a36
Dominique Thomas <dthomas@codenco.fr>
2025-02-08 17:50:30 +0100

    feat: sites

--- src/Http/Controllers/EntryController.php
+++ src/Http/Controllers/EntryController.php
@@ -14,8 +14,27 @@ class EntryController
             'start' => 'required|date',
         ]);
 
+        $model = config('horm.model.entry');
+
+        $entriesCount = $model::query()
+            ->where('created_at', '>=', $validated['start'])
+        ->count();
+        if($entriesCount == 0) {
+            return EntryResource::collection([]);
+        }
+
+
+        $comparableEntry = $model::query()
+            ->offset(min($entriesCount-1, 1000))
+            ->where('created_at', '>=', $validated['start'])
+            ->oldest()
+            ->limit(1)
+            ->first()
+        ;
+
         return EntryResource::collection(Entry::query()
             ->where('created_at', '>=', $validated['start'])
+            ->where('created_at', '<=', $comparableEntry->created_at)
             ->oldest()
             ->get()
         );
